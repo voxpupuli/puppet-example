913e97e25c6cbd7c14911b3038147ca45e7410c0
Tim Meusel <tim@bastelfreak.de>
2024-05-24 16:51:26 +0200

    output os_facts[:service_provider]

--- spec/classes/example_spec.rb
+++ spec/classes/example_spec.rb
@@ -6,9 +6,8 @@ describe 'example' do
   on_supported_os.each do |os, os_facts|
     context "on #{os}" do
       let(:facts) { os_facts }
-
-      it { is_expected.to compile.with_all_deps }
-      it { is_expected.to contain_file('/tmp/puppet-example').with_content('Hello World!') }
+      puts os_facts[:service_provider].class
+      puts os_facts[:service_provider]
     end
   end
 end
